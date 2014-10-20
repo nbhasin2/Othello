0e82acc4f69f40f1aca7afef8c7faed68049399d
Nishant Bhasin <nishantbhasin@cmail.carleton.ca>
2014-10-20 04:09:21 -0700

    Fixed invalid move issue

--- src/Othello/Game.java
+++ src/Othello/Game.java
@@ -45,7 +45,7 @@ public void playerMove(playableItem move){
 			int row = playerMove.nextInt() - 1;
 	        int col = playerMove.nextInt() - 1;
 	      
-	        if(validMove(row,col,move) == true){
+	        if(validMove(row,col,move)){
 	        	board.playField[row][col].gamePiece = move;
 	        	board.currentRow = row;
 	        	board.currentCol = col;
@@ -64,24 +64,26 @@ public void playerMove(playableItem move){
 
 public boolean validMove(int row, int col, playableItem playerPiece){
 	
-	
-	ArrayList<int[]> solutions = availableSoltuions(playerPiece);
-
+	//ArrayList<int[]> solutions = availableSoltuions(playerPiece);
+	ArrayList<String> tempSolution = availableSoltuions(playerPiece);
 	int[] move = {row,col};
     
+	String tempMove = row +"-"+col;
+	System.out.println(tempSolution);
 
-    return(solutions.contains(move));
+    return(tempSolution.contains(tempMove));
 
 
 }
 
 
-public ArrayList<int[]> availableSoltuions(playableItem playerPiece) {
+public ArrayList<String> availableSoltuions(playableItem playerPiece) {
 	
 	
 	//ArrayList<String> freeSpaces = new ArrayList<String>();
 //	System.out.println("freespaces 1" +freeSpaces);
 	ArrayList<int[]> freeSpaces = new ArrayList<int[]>();
+	ArrayList<String> temp = new ArrayList<String>();
 	int row = gameBoard.ROWS;
 	int col = gameBoard.COLS;
 	int validRow = -1;
@@ -153,7 +155,9 @@ public ArrayList<int[]> availableSoltuions(playableItem playerPiece) {
 					if(valid)
 					{
 						int[] newAdd = {validRow,validCol};
+						System.out.println("Valid Row - "+ validRow + " Valid col - " + validCol);
 						freeSpaces.add(newAdd);
+						temp.add(validRow +"-" + validCol);
 					}
 					
 					
@@ -162,7 +166,7 @@ public ArrayList<int[]> availableSoltuions(playableItem playerPiece) {
 		}
 	}
 	
-	return freeSpaces;
+	return temp;
 }
 public boolean isValid(int row,int col,int c,playableItem playerPiece)
 {
