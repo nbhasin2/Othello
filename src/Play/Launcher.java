06bad42c65d6b99a1cd36839d6bad6e8e0457d96
ZachDFK <zacharie.gauthier@gmail.com>
2014-10-19 20:08:54 -0400

    Added othello winner and laucher for all games

--- src/Play/Launcher.java
+++ src/Play/Launcher.java
@@ -1,12 +1,45 @@
 package Play;
 
+import java.util.Scanner;
+
+import Othello.Game;
 import TicTacToeConsole.TicTacToeConsole;
+
 
 public class Launcher {
 
 	public static void main(String[] args) {
+		boolean quit;
+		Scanner scan = new Scanner(System.in);
+		int ans;
+		do{
 		
-		TicTacToeConsole ticTacToeGame = new TicTacToeConsole();
-		ticTacToeGame.playTicTacToe();
+		quit = false;
+		
+		System.out.print("Enter the game you want to play,\n"
+				+ " 1- Tic Tac Toe\n"
+				+ " 2- Othello\n"
+				+ " 3- quit\n"); 
+		ans = scan.nextInt();
+		
+		if(ans == 1)
+		{
+			TicTacToeConsole ticTacToeGame = new TicTacToeConsole();
+			ticTacToeGame.playTicTacToe();
+		}
+		else if(ans == 2)
+		{
+			Game othello = new Game();
+		}
+		else if(ans ==3)
+		{
+			System.out.print("Goodbye! \n");
+			quit = true;
+		}
+		else
+		{
+			System.out.print("Not an option\n");
+		}
+		}while(!quit);
 	}
 }
