60a976ab810ca72efda24dbe9a040c442e79f4f4
ZachDFK <zacharie.gauthier@gmail.com>
2014-11-04 19:23:47 -0500

    Try for a new push

--- src/GameModel/GameConsoleInterface.java
+++ src/GameModel/GameConsoleInterface.java
@@ -2,7 +2,7 @@ package GameModel;
 
 import java.util.ArrayList;
 
-public abstract class GameConsoleInterface implements Cloneable{
+public abstract class GameConsoleInterface {
 
 	
 	
@@ -10,4 +10,5 @@ public abstract class GameConsoleInterface implements Cloneable{
 	public abstract  int evaluate();
 	public abstract void moveSet(int row, int col,int player);
 	public abstract void undoMove(int row,int col,int player);
+	public abstract boolean gameOver();
 }
